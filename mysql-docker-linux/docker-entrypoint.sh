f054aa30fb57049f9df62280a550d810363774ff
Giuseppe Maxia <g.maxia@gmail.com>
2020-08-06 11:48:22 +0200

    simplify container creation

--- mysql-docker-linux/docker-entrypoint.sh
+++ mysql-docker-linux/docker-entrypoint.sh
@@ -1,14 +1,5 @@
 #!/bin/bash
 
-if [ -x /opt/mysql/install.sh ]
-then
-    /opt/mysql/install.sh
-    echo 'export PATH=/usr/local/mysql/bin:$PATH' >> $HOME/.bashrc
-fi
-
-if [ ! -d $HOME/opt/mysql ]
-then
-    mkdir -p $HOME/opt/mysql
-fi
+# Add here initializing commands that may be needed
 
 "$@"
